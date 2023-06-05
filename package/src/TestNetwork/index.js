d44828a9ef17d15ca17758f13b6e224aff336500
gohai <gottfried.haider@gmail.com>
2023-06-05 12:55:18 +0200

    Add a new Test neural network

---
+++ package/src/TestNetwork/index.js
@@ -0,0 +1,8 @@
+import * as tf from '@tensorflow/tfjs';
+
+const testNetwork = (options) => {
+  console.log('options passed to testNetwork', options);
+  return null;
+};
+
+export default testNetwork;
